908a1d32a0771281cc8090a45cf19dbe11e82298
Marek <marek@MacBook-Air-Marek.local>
2023-09-04 21:14:31 +0200

    first commit

--- src/pages/MainPage/Main.tsx
+++ src/pages/MainPage/Main.tsx
@@ -4,7 +4,7 @@ import Search from './Search/Search'
 
 export default function Main() {
   return (
-    <div className='w-screen h-screen bg-[url("/Main_theme.png")] bg-top bg-cover bg-no-repeat flex flex-col pt-[50px]'>
+    <div className='w-screen h-screen  bg-[url("/Main_theme.png")] bg-top bg-cover bg-no-repeat flex flex-col pt-[50px]'>
         <div className='h-[100px] bg-gradient-to-b from-black to-transparent border-white'></div>
         <Claim />
         <Search />
