6626743d45c143be6b14a5c7d31831aa81c69773
Marek <marek@MacBook-Air-Marek.local>
2023-09-05 20:25:17 +0200

    first commit

--- src/pages/MainPage/Search/search.tsx
+++ src/pages/MainPage/Search/search.tsx
@@ -97,7 +97,7 @@ export default function Search() {
                             <div className="h-[50px] w-1/2 border-r flex items-center pl-[10px] relative" onClick={handleDateFocus}>
                             {/* <Calendar className='absolute bg-white border w-[280px]'/> */}
                                 <BsCalendar3 locale="en_EN" className="w-[30px] h-[30px] text-yellow-500/[0.4]"/>
-                                <input ref={inputDate} id="data" className='w-full h-[45px] pl-[5px] outline-none' type="date" value="202"></input>
+                                <input ref={inputDate} id="data" className='w-full h-[45px] pl-[5px] outline-none' type="date" defaultValue="202"></input>
                             </div>
                             <div className="h-[50px] w-1/2 flex items-center justify-center pl-[10px]">
                                 {/* <TbClockHour8 className="w-[30px] h-[30px] text-yellow-500/[0.4]"/> */}
