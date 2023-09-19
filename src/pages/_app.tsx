96555f3fa95b5084ef505fd92da50574a5c05bc0
Marek <marek@MacBook-Air-Marek.local>
2023-09-19 11:37:39 +0200

    first commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,19 +2,24 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { createContext, useState } from 'react'
 
-export const AppContext = createContext(null);
+type QueryParams = {
+  FromLocalization:any;
+  setFromLocalization(FromLocalization:any): void;
+}
+
+const AppContext = createContext<QueryParams | null>(null);
 
 export default function App({ Component, pageProps }: AppProps) {
 
       //choosed params 
-      const [FromLocalization, setFromLocalization] = useState("dupa");
+      const [FromLocalization, setFromLocalization] = useState();
       const [ToLocalization, setToLocalization] = useState();
       const [Date, setDate] = useState();
       const [Time, setTime] = useState();
   
   return (
-    // <AppContext.Provider>
+    <AppContext.Provider value={{ FromLocalization, setFromLocalization}} >
       <Component {...pageProps} />
-    // </AppContext.Provider>
+    </AppContext.Provider>
     )
 }
