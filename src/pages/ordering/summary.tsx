89a0df92631541f11be71beb1aca2195ea4cc21e
Marek <marek@MacBook-Air-Marek.local>
2023-09-19 11:39:32 +0200

    first commit

--- src/pages/ordering/summary.tsx
+++ src/pages/ordering/summary.tsx
@@ -1,13 +1,9 @@
 import React, { useEffect, Component, useContext } from 'react'
-import { AppContext } from '../_app'
 
 export default function Summary() {
 
     // const {FromLocalization, setFromLocalization} = useContext(AppContext)
     // console.log(FromLocalization)
-
-
-
 
 
   return (
