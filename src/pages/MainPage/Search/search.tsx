ad4c78b8b3314b66ab95c8f927056fd23eff55c9
Marek <marek@MacBook-Air-Marek.local>
2023-09-28 19:16:55 +0200

    first commit

--- src/pages/MainPage/Search/search.tsx
+++ src/pages/MainPage/Search/search.tsx
@@ -94,7 +94,7 @@ export default function Search() {
                 query:{
                     passengers:people,
                 },
-            },undefined, {scroll:false})
+            },undefined)
     
         }
     }
