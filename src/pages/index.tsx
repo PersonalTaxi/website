6b1b65c5ba587842aa4a78e425e8d07c6f2d8fe0
Marek <marek@MacBook-Air-Marek.local>
2023-10-13 14:56:40 +0200

    first commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,32 +1,31 @@
-import Image from 'next/image'
-import { Ubuntu } from 'next/font/google'
-import Header from './Header/header'
-import Head from 'next/head'
-import Main from './MainPage/main'
-
+import Image from "next/image";
+import { Ubuntu } from "next/font/google";
+import Header from "./Header/header";
+import Head from "next/head";
+import Main from "./MainPage/main";
 
 const rubikFonts = Ubuntu({
-  subsets: ['latin'],
-  weight:['300', '500','700']
-})
-
+  subsets: ["latin"],
+  weight: ["300", "500", "700"],
+});
 
 export default function Home() {
-
-  return (  
+  return (
     <>
       <Head>
-      <meta http-equiv='X-UA-Compatible' content='IE=Edge' ></meta>
-      <title>Your best drive</title>
-      <meta name='viewport'
-          content='width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no'></meta>
-    </Head>
-    <div className={`${rubikFonts.className}`}>
-      <main >
-        <Header />
-        <Main  />
-      </main>
-    </div>
+        <meta http-equiv="X-UA-Compatible" content="IE=Edge"></meta>
+        <title>Your best drive</title>
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no"
+        ></meta>
+      </Head>
+      <div className={`${rubikFonts.className} overflow-hidden`}>
+        <main>
+          <Header />
+          <Main />
+        </main>
+      </div>
     </>
-  )
+  );
 }
