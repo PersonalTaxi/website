73b0d53c3968d7016091e8fc867b41e2aec18a6e
Marek <marek@MacBook-Air-Marek.local>
2023-10-18 20:48:52 +0200

    first commit

--- src/pages/api/verify.tsx
+++ src/pages/api/verify.tsx
@@ -2,6 +2,6 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  const data = req;
+  const data = req.body;
   res.status(200).json({ msg: data });
 }
