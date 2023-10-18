c450de4004b4937c4ba9a17df312f5a36f33692e
Marek <marek@MacBook-Air-Marek.local>
2023-10-18 21:16:48 +0200

    first commit

--- src/pages/api/verify.tsx
+++ src/pages/api/verify.tsx
@@ -1,7 +1,10 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  const data = req.body;
-  res.status(200).json({ msg: data });
+async function handlerData(req: NextApiRequest, res: NextApiResponse) {
+  let data = await fetch("https://psbeta.vercel.app/ordering/verifyData");
+  const final = await data;
+  res.status(200).json({ msg: final });
 }
+
+export default handlerData;
