55b036997d2c15ed6b37ba125c6dd9dee608de53
Marek <marek@MacBook-Air-Marek.local>
2023-10-19 23:27:22 +0200

    first commit

--- src/pages/api/verify.tsx
+++ src/pages/api/verify.tsx
@@ -3,11 +3,11 @@ import type { NextResponse, NextRequest } from "next/server";
 
 async function handlerData(req: NextResponse, res: NextRequest) {
   await fetch("https://psbeta.vercel.app/ordering/p24callback", {
-    headers: {
-      "Content-Type": "application/json",
-      Authorization:
-        "Basic Mjc0MDc6MWI2NDdjYTJjYjRkZGI0ZmFmY2Q3NjgzZmM0MGZiYTY=",
-    },
+    // headers: {
+    //   "Content-Type": "application/json",
+    //   Authorization:
+    //     "Basic Mjc0MDc6MWI2NDdjYTJjYjRkZGI0ZmFmY2Q3NjgzZmM0MGZiYTY=",
+    // },
   });
   const final = await req.body;
   console.log(final);
