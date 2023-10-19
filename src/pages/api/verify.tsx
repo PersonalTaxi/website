0b504ab8d8d1588d07cd0e2a0bd9bff5a01fe276
Marek <marek@MacBook-Air-Marek.local>
2023-10-19 18:09:58 +0200

    first commit

--- src/pages/api/verify.tsx
+++ src/pages/api/verify.tsx
@@ -2,7 +2,7 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
 async function handlerData(req: NextApiRequest, res: NextApiResponse) {
-  fetch("https://psbeta.vercel.app/ordering/verify");
+  fetch("https://psbeta.vercel.app/ordering/p24callback");
   const final = await req.body;
   res.status(200).json({ msg: final });
 }
