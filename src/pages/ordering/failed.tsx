8f5eceedc737aa3c7e3e22cd2958a7e31909134d
Marek <marek@MacBook-Air-Marek.local>
2023-10-23 16:09:33 +0200

    first commit

--- src/pages/ordering/failed.tsx
+++ src/pages/ordering/failed.tsx
@@ -4,17 +4,19 @@ import { MdReportProblem } from "react-icons/md";
 
 export default function Succes() {
   return (
-    <div className="w-screen h-screen flex flex-col items-center justify-center">
-      <div className="text-[20px] mb-[20px]">Something went wrong...</div>
-      <div role="status" className="w-[140px] h-[140px]">
-        <MdReportProblem className="w-full h-full text-red-600" />
+    <div className="w-screen h-screen bg-[url('/Main_theme.png')] bg-top bg-cover bg-no-repeat flex justify-center items-center">
+      <div className="w-[80%] h-[35%] flex flex-col items-center justify-center mx-auto bg-white rounded-[10px]">
+        <div className="text-[20px] mb-[20px]">Something went wrong...</div>
+        <div role="status" className="w-[140px] h-[140px]">
+          <MdReportProblem className="w-full h-full text-red-600" />
+        </div>
+        <Link
+          href="https://ptbeta.vercel.app/"
+          className="border font-semibold px-[40px] py-[5px] bg-blue-500 text-white rounded-[20px]"
+        >
+          Try Again
+        </Link>
       </div>
-      <Link
-        href="https://ptbeta.vercel.app/"
-        className="border font-semibold px-[40px] py-[5px] bg-blue-500 text-white rounded-[20px]"
-      >
-        Try Again
-      </Link>
     </div>
   );
 }
--- src/pages/ordering/succes.tsx
+++ src/pages/ordering/succes.tsx
@@ -4,17 +4,22 @@ import { BsCheck2Square } from "react-icons/bs";
 
 export default function Succes() {
   return (
-    <div className="w-screen h-screen flex flex-col items-center justify-center">
-      <div className="text-[20px] mb-[20px]">Payment successful </div>
-      <div role="status" className="w-[140px] h-[140px]">
-        <BsCheck2Square className="w-full h-full text-green-600" />
+    <div className="w-screen h-screen bg-[url('/Main_theme.png')] bg-top bg-cover bg-no-repeat flex justify-center items-center">
+      <div className="w-[80%] h-[40%] flex flex-col items-center justify-center mx-auto bg-white rounded-[10px]">
+        <div className="text-[20px] mb-[20px] text-center px-[10px]">
+          <p className="font-bold">Payment successful!</p> We've send you an
+          email with order details.
+        </div>
+        <div role="status" className="w-[140px] h-[140px]">
+          <BsCheck2Square className="w-full h-full text-green-600" />
+        </div>
+        <Link
+          href="http://ptbeta.vercel.app/"
+          className="border font-semibold px-[40px] py-[5px] bg-blue-500 text-white rounded-[20px]"
+        >
+          Back to home page
+        </Link>
       </div>
-      <Link
-        href="http://ptbeta.vercel.app/"
-        className="border font-semibold px-[40px] py-[5px] bg-blue-500 text-white rounded-[20px]"
-      >
-        Back to main page
-      </Link>
     </div>
   );
 }
