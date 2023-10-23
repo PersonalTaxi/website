9b55380136b59d3a8a3d63cf9805e6956d83c794
Marek <marek@MacBook-Air-Marek.local>
2023-10-23 10:08:52 +0200

    first commit

--- src/pages/Header/header.tsx
+++ src/pages/Header/header.tsx
@@ -1,20 +1,22 @@
-import React from 'react'
-import Image from 'next/image'
-import { RxHamburgerMenu } from 'react-icons/rx'
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { RxHamburgerMenu } from "react-icons/rx";
 
 export default function Header() {
-
   return (
-    <div className='fixed z-40 h-[50px] w-screen bg-black  border-gray-900 flex items-center justify-between px-[20px]'>
-        <div>
-          <Image 
-          src="/Logo.png"
-          width={100}
-          height={100}
-          alt="taxi_krakow"
+    <div className="fixed z-40 h-[50px] w-screen bg-black  border-gray-900 flex items-center justify-between px-[20px]">
+      <div>
+        <Link href="/">
+          <Image
+            src="/Logo.png"
+            width={100}
+            height={100}
+            alt="taxi_krakow"
           ></Image>
-        </div>
-        <RxHamburgerMenu className="text-yellow-500 w-[30px] h-[30px]" />
+        </Link>
+      </div>
+      <RxHamburgerMenu className="text-yellow-500 w-[30px] h-[30px]" />
     </div>
-  )
+  );
 }
