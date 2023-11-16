1fefeaaecce0f37319af03fbbe53b1506bd2f68c
Marek <marek@MacBook-Air-Marek.local>
2023-11-16 15:03:47 +0100

    first commit

--- src/pages/MainPage/claim.tsx
+++ src/pages/MainPage/claim.tsx
@@ -3,14 +3,15 @@ import React from "react";
 export default function Claim() {
   return (
     <div className="w-screen lg:w-[1100px] mx-auto h-[25vh] lg:h-[220px] border-red-900 flex flex-col px-[20px] z-10 justify-start font-[500]">
-      <div className="text-white leading-4 lg:leading-[50px] text-[20px] lg:text-[40px]">
-        <b className="text-[30px]">Safety</b> transport from | to
+      <div className="text-white leading-4 lg:leading-[50px] text-[20px] lg:text-[30px]">
+        <b className="text-[30px] lg:text-[40px]">Safety</b> transport from | to
       </div>
       <div className="text-[36px] lg:text-[56px] leading-10 font-bold text-yellow-500">
         Krakow Airport
       </div>
-      <div className="text-white leading-6 lg:leading-[50px] text-[20px] lg:text-[40px]">
-        from | to your <b className="text-[28px]">destianation</b>
+      <div className="text-white leading-6 lg:leading-[50px] text-[20px] lg:text-[30px]">
+        from | to your{" "}
+        <b className="text-[28px] lg:text-[40px]">destianation</b>
       </div>
     </div>
   );
