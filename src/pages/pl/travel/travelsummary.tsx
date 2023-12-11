b6df985c575a3d30a72ab57fe75e898cad4fd83f
Marek <marek@MacBook-Air-Marek.local>
2023-12-11 08:25:20 +0100

    first commit

--- src/pages/pl/travel/travelsummary.tsx
+++ src/pages/pl/travel/travelsummary.tsx
@@ -190,9 +190,7 @@ export default function TravelSummary() {
                 <p>1. Zawieziemy Cię w obie strony </p>
                 <p>
                   2. Poczekamy na Ciebie podczas aż do zakończenia Twojej wycieczki, która powinna
-                  potrwać ok 3:50 h.git init git add . git commit -m "first commit" git branch -M
-                  main git remote add origin https://github.com/Marlonbrando3/personaltaxi.git git
-                  push -u origin main
+                  potrwać ok 3:50 h.git init git add
                 </p>
                 <p>
                   3. Na wycieczkie jedzie łącznie <b>{persons} </b>osoby/osób. Dlatego pojedziecie
