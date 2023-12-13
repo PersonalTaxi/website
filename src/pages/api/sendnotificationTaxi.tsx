798855ad7de2e7f10c5fc66c261dd596e4814ba0
Marek <marek@MacBook-Air-Marek.local>
2023-12-13 15:01:19 +0100

    first commit

--- src/pages/api/sendnotificationTaxi.tsx
+++ src/pages/api/sendnotificationTaxi.tsx
@@ -75,7 +75,7 @@ export default async function SendnotificationTaxi(
   };
 
   const mailToCustomerDataPL = {
-    from: "Potwierdzenie zamówienia - Personal Taxi",
+    from: req.body.email,
     to: req.body.email,
     subject: `Zamówienie transferu / Taxi`,
     text: "Hello. This email is for your email verification.",
