53be7d1c7564188ae350f61af2531864cfa61b07
Marek <marek@MacBook-Air-Marek.local>
2023-12-14 18:46:46 +0100

    first commit

--- src/middleware.tsx
+++ src/middleware.tsx
@@ -1,5 +1,9 @@
 import { NextRequest, NextResponse } from "next/server";
 
+export const config = {
+  matcher: "/",
+};
+
 export async function middleware(req: NextRequest) {
   const { nextUrl: url, geo } = req;
   const country = geo.country || "US";
